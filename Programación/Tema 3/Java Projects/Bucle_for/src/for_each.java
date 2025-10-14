225db1745fb958d33e359c1d4c1cda0e89b1950f
obiticadev <obitica.dev@gmail.com>
2025-10-14 19:36:21 +0200

    Ejercicios de programación añadidos: For

---
+++ Programación/Tema 3/Java Projects/Bucle_for/src/for_each.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class for_each {
+    public static void main(String[] args) throws Exception {
+        int[]num = {1,2,3,7,8,9,10};
+         for(int select : num){
+            System.out.println("Número: " + select);
+         }
+        }
+    }
