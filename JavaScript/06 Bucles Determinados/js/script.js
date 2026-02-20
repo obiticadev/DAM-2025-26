b1e963d184e7e5f55999d00ab887e694c8b98f58
obiticadev <obitica.dev@gmail.com>
2026-02-20 23:48:33 +0100

    Ejercicios

--- JavaScript/06 Bucles Determinados/js/script.js
+++ JavaScript/06 Bucles Determinados/js/script.js
@@ -10,11 +10,72 @@ const potenciaCubo = (array) => {
     let arrayCubo = [];
     let arrayCompleto = [];
     for (let i = 0; i < array.length; i++) {
-        arrayCuadrado.push(Math.pow(array[i],2));
-        arrayCubo.push(Math.pow(array[i],3));
+        arrayCuadrado.push(Math.pow(array[i], 2));
+        arrayCubo.push(Math.pow(array[i], 3));
         arrayCompleto.push(`Número: ${array[i]} - Cuadrado: ${arrayCuadrado[i]} - Cubo: ${arrayCubo[i]}\n`);
+    }
+    console.log(arrayCompleto.join(''));
+
+}
+
+potenciaCubo([1, 2, 3, 5, 8])
+
+/**
+ * 
+ * @param {number[]} array 
+ */
+
+const potenciaCuboForOf = (array) => {
+    let arrayCompleto = [];
+    for (const element of array) {
+        let cuadrado = (Math.pow(element, 2));
+        let cubo = (Math.pow(element, 3));
+        arrayCompleto.push(`Número: ${element} - Cuadrado: ${cuadrado} - Cubo: ${cubo}\n`);
     }
     console.log(arrayCompleto.join(''));
 }
 
-potenciaCubo([1,2,3,5,8])
+potenciaCuboForOf([2, 3, 4, 5]);
+
+console.log('Ejercicio 2: Resaltador de Vocales');
+
+/**
+ * 
+ * @param {string} word 
+ */
+
+const formatMayus = (word) => {
+    const regex = /[aeiouáéíóú]/;
+    let arrayFormat = [];
+    for (const character of word) {
+        if (regex.test(character)) {
+            arrayFormat.push(character.toUpperCase());
+        } else {
+            arrayFormat.push(character);
+        }
+    }
+    console.log(arrayFormat.join(''));
+}
+
+formatMayus('casa esto es una prueba para verificar la utilidad de la función');
+
+/**
+ * 
+ * @param {string} word 
+ */
+
+const formatMayusNormal = (word) => {
+    const regex = /[aeiou]/;
+    let arrayFormat = [];
+    for (let i = 0; i < word.length; i++) {
+        if (regex.test(word.split('')[i])) {
+            arrayFormat.push(word.split('')[i].toUpperCase());
+        } else {
+            arrayFormat.push(word.split('')[i]);
+        }
+
+    }
+    console.log(arrayFormat.join(''));
+}
+
+formatMayusNormal('Esto es una pruebas');
