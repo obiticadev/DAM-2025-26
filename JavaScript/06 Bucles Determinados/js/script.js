7665378da333e04bdeb3658df3fa375a83a962cc
obiticadev <obitica.dev@gmail.com>
2026-02-21 22:32:50 +0100

    Ejercicios JavaScript

--- JavaScript/06 Bucles Determinados/js/script.js
+++ JavaScript/06 Bucles Determinados/js/script.js
@@ -79,3 +79,250 @@ const formatMayusNormal = (word) => {
 }
 
 formatMayusNormal('Esto es una pruebas');
+
+console.log('Ejercicio 3: Contador de frencuencia de vocales');
+
+/**
+ * 
+ * @param {string} phrase 
+ */
+
+const contadorVocales = (phrase) => {
+    const a = /[aeiou]/i;
+    let arrayContadorVocales = [0, 0, 0, 0, 0];
+    let arrayPhrase = phrase.split('');
+
+    for (let i = 0; i < arrayPhrase.length; i++) {
+        if (a.test(arrayPhrase[i])) {
+            switch (arrayPhrase[i].toLowerCase()) {
+                case 'a':
+                    arrayContadorVocales[0]++;
+                    break;
+                case 'e':
+                    arrayContadorVocales[1]++;
+                    break;
+                case 'i':
+                    arrayContadorVocales[2]++;
+                    break;
+                case 'o':
+                    arrayContadorVocales[3]++;
+                    break;
+                case 'u':
+                    arrayContadorVocales[4]++;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    console.log(arrayContadorVocales.join(' '));
+}
+
+contadorVocales('Esto es una prueba para que me digas cuantas vocales estás capturando')
+
+/**
+ * 
+ * @param {string} phrase 
+ */
+
+const contadorVocalesForOf = (phrase) => {
+    const a = /[aeiou]/i;
+    let arrayContadorVocales = [0, 0, 0, 0, 0];
+    let arrayPhrase = phrase.split('');
+
+    for (const element of arrayPhrase) {
+        if (a.test(element)) {
+            switch (element.toLowerCase()) {
+                case 'a':
+                    arrayContadorVocales[0]++;
+                    break;
+                case 'e':
+                    arrayContadorVocales[1]++;
+                    break;
+                case 'i':
+                    arrayContadorVocales[2]++;
+                    break;
+                case 'o':
+                    arrayContadorVocales[3]++;
+                    break;
+                case 'u':
+                    arrayContadorVocales[4]++;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    console.log(arrayContadorVocales.join(' '));
+}
+
+contadorVocalesForOf('Esto es una prueba para que me digas cuantas vocales estás capturando')
+
+
+console.log('Ejercicio 4: Generador y Buscador Aleatorio');
+
+/**
+ * 
+ * @param {number[]} arrayDelimitado 
+ */
+
+const generadorYBuscador = (arrayDelimitado) => {
+    for (let i = 0; i < arrayDelimitado.length; i++) {
+        const numGenerator = Math.floor(Math.random() * (arrayDelimitado[i] + 1));
+        console.log(numGenerator);
+        for (let j = 0; j < arrayDelimitado.length; j++) {
+            if (numGenerator === arrayDelimitado[j]) {
+                console.log(`El número ${numGenerator} se encuentra en la posición ${j + 1}`);
+            }
+        }
+    }
+}
+
+
+
+generadorYBuscador([4, 8, 2, 7])
+console.log('\nSEGUNDA PARTE\n\n');
+/**
+ * 
+ * @param {number[]} arrayDelimitado 
+ */
+
+const generadorYBuscadorForOf = (arrayDelimitado) => {
+    for (const element of arrayDelimitado) {
+        const numGenerator = Math.floor(Math.random() * (element + 1));
+        console.log(numGenerator);
+        let contador = 0;
+        for (const element2 of arrayDelimitado) {
+            if (numGenerator === element2) {
+                console.log(`El número ${numGenerator} se encuentra en la posición ${contador + 1}`);
+            }
+            contador++;
+        }
+    }
+}
+
+generadorYBuscadorForOf([4, 8, 2, 7]);
+
+console.log('Ejercicio 5 : Inversión de Cadena (Manual)');
+
+/**
+ * 
+ * @param {string} word 
+ */
+
+const reversePhrase = (word) => {
+    let arrayReverse = [];
+    let copiaWordArray = word.split('');
+    for (let i = 0; i < word.length; i++) {
+        let elementoRetirado = copiaWordArray.pop();
+        arrayReverse.push(elementoRetirado);
+    }
+    console.log(arrayReverse.join(''));
+}
+
+reversePhrase('Esto es una prueba')
+
+/**
+ * 
+ * @param {string} word 
+ */
+
+const reversePhraseForOf = (word) => {
+    let arrayReverse = [];
+    let copiaWordArray = word.split('');
+
+    for (const element of word) {
+        let elementoRetirado = copiaWordArray.pop();
+        arrayReverse.push(elementoRetirado);
+    }
+    console.log(arrayReverse.join(''));
+}
+
+reversePhraseForOf('Esto es una prueba');
+
+console.log('Ejercicio 6: Clasificador de Pares e Impares Aleatorios');
+
+/**
+ * 
+ * @param {number[]} array 
+ */
+
+const clasificadorParesImpares = (array) => {
+    let arrayPar = [];
+    let arrayImpar = [];
+    for (let i = 0; i < array.length; i++) {
+        const numGenerated = Math.floor(Math.random() * (array[i] + 1))
+        if (numGenerated % 2 === 0) {
+            arrayPar.push(numGenerated);
+        } else {
+            arrayImpar.push(numGenerated);
+        }
+    }
+    console.log(`Array original: ${array.join(' ')}\nArray de pares: ${arrayPar.join(' ')}\nArray de impares: ${arrayImpar.join(' ')}`);
+}
+
+clasificadorParesImpares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+/**
+ * 
+ * @param {number[]} array 
+ */
+
+const clasificadorParesImparesForOf = (array) => {
+    let arrayPar = [];
+    let arrayImpar = [];
+
+    for (const element of array) {
+        const numGenerated = Math.floor(Math.random() * (element + 1))
+        if (numGenerated % 2 === 0) {
+            arrayPar.push(numGenerated);
+        } else {
+            arrayImpar.push(numGenerated);
+        }
+    }
+    console.log(`Array original: ${array.join(' ')}\nArray de pares: ${arrayPar.join(' ')}\nArray de impares: ${arrayImpar.join(' ')}`);
+}
+
+clasificadorParesImparesForOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+console.log('Ejercicio 7: Extractor de extremos de palabras');
+
+/**
+ * 
+ * @param {string[]} arrayWord 
+ */
+
+const extractCharacter = (arrayWord) => {
+    let arrayFinal = [];
+    for (let i = 0; i < arrayWord.length; i++) {
+        const first = arrayWord[i].split('').shift();
+        const end = arrayWord[i].split('').pop();
+        arrayFinal.push(first);
+        arrayFinal.push(end);
+    }
+    console.log(arrayFinal.join(' ').toUpperCase());
+}
+
+extractCharacter(['Hola', 'Mundo', 'qué', 'tal', 'estás'])
+
+/**
+ * 
+ * @param {string[]} arrayWord 
+ */
+
+const extractCharacterForOf = (arrayWord) => {
+    let arrayFinal = [];
+    for (const element of arrayWord) {
+        const first = element.split('').shift();
+        const end = element.split('').pop();
+        arrayFinal.push(first);
+        arrayFinal.push(end);
+    }
+    console.log(arrayFinal.join(' ').toUpperCase());
+}
+
+extractCharacterForOf(['Hola', 'Mundo', 'qué', 'tal', 'estás'])
