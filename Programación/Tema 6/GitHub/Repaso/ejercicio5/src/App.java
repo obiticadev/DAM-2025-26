803202a4c4973a2de7f507d9bd8affc92c6ee2a5
obiticadev <obitica.dev@gmail.com>
2026-02-27 17:32:20 +0100

    Ejercicios programacion 21

--- Programación/Tema 6/GitHub/Repaso/ejercicio5/src/App.java
+++ Programación/Tema 6/GitHub/Repaso/ejercicio5/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean continuar = false;
+
+        System.out.println("=== BIBLIOTECA ===");
+        do {
+            System.out.println("""
+                    MENÚ
+                    (1) => Registrar libro
+                    (2) => Buscar libro
+                    (3) => Modificar libro
+                    (4) => Listar libros
+                    (5) => Registrar reserva
+                    (6) => Buscar reserva
+                    (7) => Modificar reserva
+                    (8) => Listar reservas
+                        """);
+
+        } while (!continuar);
     }
 }
--- Programación/Tema 6/GitHub/Repaso/ejercicio5/src/DAO/GesionReservasDAO.java
+++ Programación/Tema 6/GitHub/Repaso/ejercicio5/src/DAO/GesionReservasDAO.java
@@ -18,8 +18,11 @@ public class GesionReservasDAO {
     private void cargarDatos() {
         Reservas r1 = new Reservas("Oliver", LocalDate.of(2025, 12, 5), new Libros("12", "Harry Potter", "Stuart"));
         Reservas r2 = new Reservas("Ruben", LocalDate.of(2025, 5, 12), new Libros("23", "Luisito", "Marta"));
+        Reservas r3 = new Reservas("Martín", LocalDate.of(2015, 11, 8), new Libros("56", "Merlín", "HBO"));
+
         dao.getListaReservas().add(r1);
         dao.getListaReservas().add(r2);
+        dao.getListaReservas().add(r3);
     }
 
     public ArrayList<Reservas> devolverDatos() {
