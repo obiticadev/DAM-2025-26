7122a4c911d6131073893bcdda056e058c3a5992
obiticadev <obitica.dev@gmail.com>
2026-03-20 23:00:07 +0100

    Ejercicios

--- Programación/Tema 9/GitHub/ejercicio6/src/App.java
+++ Programación/Tema 9/GitHub/ejercicio6/src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -25,13 +26,19 @@ public class App {
         System.out.println(separador);
         ejercicio3();
         System.out.println(separador);
-        ejercicio4y5();
+        ejercicio4();
+        System.out.println(separador);
+        ejercicio5();
         System.out.println(separador);
         ejercicio6();
         System.out.println(separador);
         ejercicio7();
         System.out.println(separador);
         ejercicio8();
+        System.out.println(separador);
+        ejercicio9();
+        System.out.println(separador);
+        ejercicio10();
         System.out.println(separador);
 
     }
@@ -70,14 +77,14 @@ public class App {
     private static void ejercicio3() {
         System.out.println("Ejercicio 3:");
         mapa = new HashMap<>(Map.of("Stuart", 5, "Lopez", 78, "Lucas", 1, "Santi", 18, "Luis", 18));
-        for (Map.Entry tupla : mapa.entrySet()) {
+        for (Map.Entry<String, Integer> tupla : mapa.entrySet()) {
             System.out.println(tupla.getKey() + ", " + tupla.getValue());
         }
 
     }
 
-    private static void ejercicio4y5() {
-        System.out.println("Ejercicio 4 y 5:");
+    private static void ejercicio4() {
+        System.out.println("Ejercicio 4:");
         String nombre = "Lopez";
         if (mapa.containsKey(nombre)) {
             System.out.println("La edad de " + nombre + " es " + mapa.get(nombre));
@@ -86,11 +93,11 @@ public class App {
         }
     }
 
-    private static void ejercicio6() {
-        System.out.println("Ejercicio 6:");
+    private static void ejercicio5() {
+        System.out.println("Ejercicio 5:");
         int count = 0;
         Integer num = 18;
-        for (Map.Entry tupla : mapa.entrySet()) {
+        for (Map.Entry<String, Integer> tupla : mapa.entrySet()) {
             if (tupla.getValue().equals(num)) {
                 count++;
             }
@@ -98,8 +105,8 @@ public class App {
         System.out.println("Existen " + count + " con " + num + " años");
     }
 
-    private static void ejercicio7() {
-        System.out.println("Ejercicio 7:");
+    private static void ejercicio6() {
+        System.out.println("Ejercicio 6:");
         listaPersonas = new LinkedList<>(List.of(new Persona("Y3142406X", "Oliver", 24),
                 new Persona("34567890", "Luis", 34), new Persona("1234", "Paco", 12), new Persona("1234", "Paco", 12),
                 new Persona("1234", "Lucas", 12), new Persona("123", "Paco", 12), new Persona("123", "Santi", 34)));
@@ -113,8 +120,8 @@ public class App {
         }
     }
 
-    private static void ejercicio8() {
-        System.out.println("Ejercicio 8:");
+    private static void ejercicio7() {
+        System.out.println("Ejercicio 7:");
         String frase = "Esta Esta Esta es es mi mi prueba prueba prueba prueba final";
         String[] fraseArray = frase.split(" ");
         contadorMapeado = new HashMap<>();
@@ -128,8 +135,34 @@ public class App {
                 contadorMapeado.put(palabra, a);
             }
         }
-        for (Map.Entry tupla : contadorMapeado.entrySet()) {
+        for (Map.Entry<String, Integer> tupla : contadorMapeado.entrySet()) {
             System.out.println(tupla.toString());
         }
+    }
+
+    private static void ejercicio8() {
+        System.out.println("Ejercicio 8:");
+        if (!mapa.isEmpty()) {
+            Map.Entry<String, Integer> maxEntry = Collections.max(mapa.entrySet(), Map.Entry.comparingByValue());
+            System.out.println("El ganador es: " + maxEntry.getKey() + " con " + maxEntry.getValue() + " años");
+        }
+    }
+
+    private static void ejercicio9() {
+        System.out.println("Ejercicio 9:");
+        String nombre = "Prueba 2";
+        Integer num = 216;
+        if (mapa.putIfAbsent(nombre, num) == null) {
+            System.out.println("Nueva entrada: " + nombre + " = " + mapa.get(nombre));
+        }
+    }
+
+    private static void ejercicio10() {
+        System.out.println("Ejercicio 10");
+        Integer contador = 0;
+        for (Map.Entry<String, Integer> tupla : mapa.entrySet()) {
+            contador += tupla.getValue();
+        }
+        System.out.println("La suma total es de " + contador);
     }
 }
