9e825daf3ae155c1f2187d7f083224100f7a8db0
obiticadev <obitica.dev@gmail.com>
2026-04-08 21:12:29 +0200

    Ajustes

--- Programación/Tema 9/GitHub/Repaso/ejercicio 3/src/App.java
+++ Programación/Tema 9/GitHub/Repaso/ejercicio 3/src/App.java
@@ -1,9 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class App {
